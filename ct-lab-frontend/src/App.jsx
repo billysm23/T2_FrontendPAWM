18c8e6c7742393c03101aa279261f49aa8a62709
billysm23 <bllysm23@gmail.com>
2024-11-16 06:09:10 +0700

    delete helmet

--- ct-lab-frontend/src/App.jsx
+++ ct-lab-frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React, { Suspense, lazy } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
@@ -7,11 +6,6 @@ import SessionGuard from './components/SessionGuard';
 import ScrollToTop from './components/common/ScrollToTop';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
-import LessonDetail from './components/lesson/LessonDetail';
-import ContentSection from './components/lesson/section/ContentSection';
-import OverviewSection from './components/lesson/section/OverviewSection';
-import QuizSection from './components/lesson/section/QuizSection';
-import ResourcesSection from './components/lesson/section/ResourcesSection';
 import { AuthProvider } from './context/AuthContext';
 import { ProgressProvider } from './context/ProgressContext';
 import { ThemeProvider } from './context/ThemeContext';
@@ -21,6 +15,11 @@ import Home from './pages/Home';
 import Lesson from './pages/Lesson';
 import Login from './pages/Login';
 import Register from './pages/Register';
+const LessonDetail = lazy(() => import('./components/lesson/LessonDetail'));
+const OverviewSection = lazy(() => import('./components/lesson/section/OverviewSection'));
+const ContentSection = lazy(() => import('./components/lesson/section/ContentSection'));
+const ResourcesSection = lazy(() => import('./components/lesson/section/ResourcesSection'));
+const QuizSection = lazy(() => import('./components/lesson/section/QuizSection'));
 
 function App() {
     return (
@@ -31,37 +30,30 @@ function App() {
                         <ScrollToTop/>
                         <Toaster position="top-right" />
                         <div className="app">
-                            <Helmet>
-                                <title>CT Lab</title>
-                            </Helmet>
+                                {/* <title>CT Lab</title> */}
                             <Navbar />
                             <main>
-                                <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/about" element={<About />} />
-                                <Route path="/login" element={<Login />} />
-                                <Route path="/register" element={<Register />} />
-                                <Route path="/lesson" element={<Lesson />} />
-                                <Route path="/lesson/:id/*" element={<LessonDetail />} />
-                                    <Route path="overview" element={<OverviewSection />} />
-                                    <Route path="content" element={<ContentSection />} />
-                                    <Route path="resources" element={<ResourcesSection />} />
-                                    <Route path="quiz" element={
-                                        <SessionGuard>
-                                            <QuizSection />
-                                        </SessionGuard>
-                                    } />
-                                <Route path="/coming-soon" element={<ComingSoon />} />
-                                {/* Quiz requires authentication */}
-                                <Route
-                                    path="quiz"
-                                    element={
-                                    <PrivateRoute>
-                                        <QuizSection />
-                                    </PrivateRoute>
-                                    }
-                                />
-                                </Routes>
+                                <Suspense fallback={<div>Loading...</div>}>
+                                    <Routes>
+                                    <Route path="/" element={<Home />} />
+                                    <Route path="/about" element={<About />} />
+                                    <Route path="/login" element={<Login />} />
+                                    <Route path="/register" element={<Register />} />
+                                    <Route path="/lesson" element={<Lesson />} />
+                                    <Route path="/lesson/:id/*" element={<LessonDetail />} />
+                                        <Route path="overview" element={<OverviewSection />} />
+                                        <Route path="content" element={<ContentSection />} />
+                                        <Route path="resources" element={<ResourcesSection />} />
+                                        <Route path="quiz" element={
+                                            <SessionGuard>
+                                                <PrivateRoute>
+                                                    <QuizSection />
+                                                </PrivateRoute>
+                                            </SessionGuard>
+                                        } />
+                                    <Route path="/coming-soon" element={<ComingSoon />} />
+                                    </Routes>
+                                </Suspense>
                             </main>
                             <Footer />
                         </div>
